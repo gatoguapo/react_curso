35479e02d2000b8722b24a56b96abd6a16c22675
gatoguapo <victor._espinoza@hotmail.com>
2024-03-12 21:17:02 -0700

    h1 Bienvenido

--- Sistema-Profesores/src/App.jsx
+++ Sistema-Profesores/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './assets/css/App.css'
+import { AppPantallaInicio } from './assets/vistas/AppPantallaInicio'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <AppPantallaInicio>
+      
+    </AppPantallaInicio>
   )
 }
 
---
+++ Sistema-Profesores/src/assets/vistas/AppPantallaInicio.jsx
@@ -0,0 +1,11 @@
+import '../css/AppPantallaInicio.css'
+
+export function AppPantallaInicio() {
+    return (
+        <body>
+            <div>
+                <h1>Bienvenido</h1>
+            </div>
+        </body>
+    )
+}
