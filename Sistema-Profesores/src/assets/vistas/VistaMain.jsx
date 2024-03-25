678a33f1578cc1f83dd61e0a3bc55495bc8e6c1e
gatoguapo <victor._espinoza@hotmail.com>
2024-03-25 01:33:57 -0700

    carpeta components creada

--- Sistema-Profesores/src/assets/vistas/VistaMain.jsx
+++ Sistema-Profesores/src/assets/vistas/VistaMain.jsx
@@ -3,8 +3,9 @@ import "../css/VistaMain.css"
 export function VistaMain() {
     return (
         <>
-            <h2 className="main-title">¿Qué quieres hacer?</h2>
+            <h2 className="main-title">¿Qué quierés hacer?</h2>
             <div className="main-layer">
+                
             </div>
         </>
     )
