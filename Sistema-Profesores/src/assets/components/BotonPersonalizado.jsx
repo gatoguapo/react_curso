4f9c8f07c8d4a38af0cf7a743e262c4d82cf88b7
gatoguapo <victor._espinoza@hotmail.com>
2024-03-27 02:38:55 -0700

    botonesPersonalizados con bordes

--- Sistema-Profesores/src/assets/components/BotonPersonalizado.jsx
+++ Sistema-Profesores/src/assets/components/BotonPersonalizado.jsx
@@ -1,8 +1,8 @@
 import "../css/BotonPersonalizado.css"
 
-export function BotonPersonalizado({text, xmlns, viewBox, d, width}) {
+export function BotonPersonalizado({text, xmlns, viewBox, d, width, className}) {
     return (
-        <button className="persBtn">
+        <button className={className}>
             <svg xmlns={xmlns} viewBox={viewBox} width={width} className="persBtn-svg">
                 <path d={d}/>
             </svg>
