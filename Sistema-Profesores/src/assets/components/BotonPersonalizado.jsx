41b4b3bf84ffc4080b389bfd8e579b0806c2e525
gatoguapo <victor._espinoza@hotmail.com>
2024-03-27 02:44:11 -0700

    botonesPersonalizados iconos color

--- Sistema-Profesores/src/assets/components/BotonPersonalizado.jsx
+++ Sistema-Profesores/src/assets/components/BotonPersonalizado.jsx
@@ -1,9 +1,9 @@
 import "../css/BotonPersonalizado.css"
 
-export function BotonPersonalizado({text, xmlns, viewBox, d, width, className}) {
+export function BotonPersonalizado({text, className, xmlns, viewBox, d, svgWidth, svgColor}) {
     return (
         <button className={className}>
-            <svg xmlns={xmlns} viewBox={viewBox} width={width} className="persBtn-svg">
+            <svg xmlns={xmlns} viewBox={viewBox} width={svgWidth} className="persBtn-svg" fill={svgColor}>
                 <path d={d}/>
             </svg>
             {text}
