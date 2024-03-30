497e38ea97426fc6e9efd44d20aeca0c8e165d41
gatoguapo <victor._espinoza@hotmail.com>
2024-03-29 23:56:11 -0700

    IconoLibro funcion

--- Sistema-Profesores/src/assets/vistas/AppPantallaInicio.jsx
+++ Sistema-Profesores/src/assets/vistas/AppPantallaInicio.jsx
@@ -1,6 +1,6 @@
 import '../css/AppPantallaInicio.css'
 import { BotonPersonalizado } from '../componentes/BotonPersonalizado'
-import { IconoPersona } from '../componentes/Iconos'
+import { IconoPersona, IconoLibro } from '../componentes/Iconos'
 
 export function AppPantallaInicio() {
     return (
@@ -22,12 +22,8 @@ export function AppPantallaInicio() {
                         </BotonPersonalizado> 
                         <BotonPersonalizado 
                             text="Agregar materias"
-                            svgWidth="20px"
                             className="persBtn"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 448 512"
-                            svgColor="#1F2544"
-                            d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
+                            icono={<IconoLibro></IconoLibro>}
                             >
                         </BotonPersonalizado>
                         <BotonPersonalizado 
